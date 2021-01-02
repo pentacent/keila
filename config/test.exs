ffb4c8669efac5a69d8e7b67fb1e38aef5c34fc4
Philipp <git@pentacent.com>
2021-01-02 16:04:10 +0100

    Configure test env for GitHub actions

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :keila, Keila.Repo,
   username: "postgres",
   password: "postgres",
   database: "keila_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: "postgres",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
