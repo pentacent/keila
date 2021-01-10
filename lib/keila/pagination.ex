1a94ed73fc813cf26abbf554ca2e12116f2a15a2
Philipp <git@pentacent.com>
2021-01-10 15:42:49 +0100

    Add Pagination module

---
+++ lib/keila/pagination.ex
@@ -0,0 +1,38 @@
+defmodule Keila.Pagination do
+  @moduledoc """
+  Module for paginating Ecto Queries.
+  """
+
+  defstruct [:page, :data, :page_count]
+  @type t :: %__MODULE__{}
+  @type t(type) :: %__MODULE__{data: type}
+
+  import Ecto.Query
+  require Ecto.Query
+
+  @doc """
+  Paginates a query and retrieves data from Repo.
+
+  ## Options:
+  - `:page` - the page that should be retrieved. Defaults to `0`.
+  - `:page_size` - the number of records per page. Defaults to `10`.
+  """
+  @spec paginate(Ecto.Query.t(), page: integer(), page_size: integer()) :: t()
+  def paginate(query, opts \\ []) do
+    page = Keyword.get(opts, :page, 0)
+    page_size = Keyword.get(opts, :page_size, 10)
+    page_count = Keila.Repo.aggregate(query, :count, :id) / page_size
+
+    data =
+      query
+      |> limit(^page_size)
+      |> offset(^(page * page_size))
+      |> Keila.Repo.all()
+
+    %__MODULE__{
+      page: page,
+      data: data,
+      page_count: page_count
+    }
+  end
+end
---
+++ test/keila/pagination_test.exs
@@ -0,0 +1,22 @@
+defmodule Keila.PaginationTest do
+  use ExUnit.Case, async: true
+  import Keila.Factory
+
+  alias Keila.{Pagination, Auth.User, Repo}
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+  end
+
+  @tag :paginate
+  test "paginate queries" do
+    user = insert_n!(:user, 100, fn n -> %{email: "#{n}@example.com"} end)
+
+    assert page = %Pagination{} = Pagination.paginate(User, page_size: 10)
+    assert page.page_count == 10
+    assert page.page == 0
+    assert "1@example.com" in Enum.map(page.data, & &1.email)
+    assert "10@example.com" in Enum.map(page.data, & &1.email)
+    refute "11@example.com" in Enum.map(page.data, & &1.email)
+  end
+end
