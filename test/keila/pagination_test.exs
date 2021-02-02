93736f3bc5b43e6a75e331b628ce352f6bd67368
Philipp <git@pentacent.com>
2021-02-02 21:54:24 +0100

    Avoid relying on inherent database order in pagination test

--- test/keila/pagination_test.exs
+++ test/keila/pagination_test.exs
@@ -1,20 +1,25 @@
 defmodule Keila.PaginationTest do
   use Keila.DataCase, async: true
   alias Keila.{Pagination, Auth.User}
+  import Ecto.Query
+  require Ecto.Query
 
   @tag :pagination
   test "paginate queries" do
-    insert_n!(:user, 101, fn n -> %{email: "#{n}@example.com"} end)
+    insert_n!(:user, 101, fn _n ->
+      %{email: "#{String.pad_leading(to_string(n), 3, "0")}@example.com"}
+    end)
 
-    assert page = %Pagination{} = Pagination.paginate(User, page_size: 10)
+    query = from(u in User, order_by: u.email)
+    assert page = %Pagination{} = Pagination.paginate(query, page_size: 10)
     assert page.page_count == 11
     assert page.page == 0
-    assert "1@example.com" in Enum.map(page.data, & &1.email)
-    assert "10@example.com" in Enum.map(page.data, & &1.email)
-    refute "11@example.com" in Enum.map(page.data, & &1.email)
+    assert "001@example.com" in Enum.map(page.data, & &1.email)
+    assert "010@example.com" in Enum.map(page.data, & &1.email)
+    refute "011@example.com" in Enum.map(page.data, & &1.email)
 
     assert page = %Pagination{} = Pagination.paginate(User, page_size: 10, page: 1)
     assert page.page == 1
-    assert "11@example.com" in Enum.map(page.data, & &1.email)
+    assert "011@example.com" in Enum.map(page.data, & &1.email)
   end
 end
