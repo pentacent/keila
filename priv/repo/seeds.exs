756971a1b128e441089c182c6c098bbcde8e39fc
Philipp <git@pentacent.com>
2021-02-23 11:12:34 +0100

    Create random root user/password if not provided

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,30 +12,45 @@ if Keila.Repo.all(Auth.Group) == [] do
   permission = Keila.Repo.insert!(%Auth.Permission{name: "administer_keila"})
   Keila.Repo.insert!(%Auth.RolePermission{role_id: role.id, permission_id: permission.id})
 
-  email = System.get_env("KEILA_USER")
-  password = System.get_env("KEILA_PASSWORD")
+  email =
+    case System.get_env("KEILA_USER") do
+      email when email not in ["", nil] ->
+        email
 
-  if email not in ["", nil] and password not in ["", nil] do
-    case Keila.Auth.create_user(%{email: email, password: password}) do
-      {:ok, user} ->
-        Keila.Auth.activate_user(user.id)
-        Keila.Auth.add_user_group_role(user.id, group.id, role.id)
-        Logger.info("Created root user with #{email}")
-
-      {:error, changeset} ->
-        Keila.ReleaseTasks.rollback(0)
-
-        errors =
-          Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
-            Enum.reduce(opts, msg, fn {key, value}, acc ->
-              String.replace(acc, "%{#{key}}", to_string(value))
-            end)
-          end)
-
-        Logger.error("Failed to create root user: #{inspect(errors)}")
-        Logger.flush()
-        System.halt(1)
+      _empty ->
+        Logger.warn("KEILA_USER not set. Creating root user with email: root@localhost")
     end
+
+  password =
+    case System.get_env("KEILA_PASSWORD") do
+      password when password not in ["", nil] ->
+        password
+
+      _empty ->
+        password = :crypto.strong_rand_bytes(24) |> Base.url_encode64()
+        Logger.warn("KEILA_PASSWORD not set. Setting random root user password: #{password}")
+        password
+    end
+
+  case Keila.Auth.create_user(%{email: email, password: password}) do
+    {:ok, user} ->
+      Keila.Auth.activate_user(user.id)
+      Keila.Auth.add_user_group_role(user.id, group.id, role.id)
+      Logger.info("Created root user with #{email}")
+
+    {:error, changeset} ->
+      Keila.ReleaseTasks.rollback(0)
+
+      errors =
+        Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+          Enum.reduce(opts, msg, fn {key, value}, acc ->
+            String.replace(acc, "%{#{key}}", to_string(value))
+          end)
+        end)
+
+      Logger.error("Failed to create root user: #{inspect(errors)}")
+      Logger.flush()
+      System.halt(1)
   end
 else
   Logger.info("Database already populated, not populating database.")
