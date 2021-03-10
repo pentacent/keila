d4ae63d95e4b70c3d4edeb77a03bff2ea7bd5f3b
Philipp <git@pentacent.com>
2021-03-10 16:23:54 +0100

    Add tzdata

--- config/config.exs
+++ config/config.exs
@@ -64,6 +64,9 @@ config :keila, Oban,
      ]}
   ]
 
+# Use Timezone database
+config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
