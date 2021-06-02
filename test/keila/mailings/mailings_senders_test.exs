705fa474ee26315ef218b61e1696777e9c60e41e
Philipp <git@pentacent.com>
2021-06-02 10:31:42 +0200

    Add callbacks to SenderAdapter behaviour
    This adds the following optional callbacks to SenderAdapters:
    - after_create
    - after_update
    - before_delete

--- test/keila/mailings/mailings_senders_test.exs
+++ test/keila/mailings/mailings_senders_test.exs
@@ -4,26 +4,81 @@ defmodule Keila.Mailings.SenderTest do
   alias Mailings.Sender
 
   @tag :mailings
-  test "Create senders" do
-    group = insert!(:group)
-    project = insert!(:project, group: group)
+  describe "Creating senders" do
+    @tag :mailings
+    test "through the API" do
+      group = insert!(:group)
+      project = insert!(:project, group: group)
 
-    {:ok, %Sender{}} =
-      Mailings.create_sender(project.id, %{
-        "name" => "Sender",
-        "from_email" => "foo@bar.com",
-        "config" => %{
-          "type" => "smtp",
-          "smtp_username" => "foo",
-          "smtp_password" => "foo",
-          "smtp_relay" => "example.com"
-        }
-      })
+      {:ok, %Sender{}} = Mailings.create_sender(project.id, params(:mailings_sender))
+    end
+
+    @tag :mailingsx
+    test "fails when there is a callback error" do
+      group = insert!(:group)
+      project = insert!(:project, group: group)
+
+      params =
+        params(:mailings_sender)
+        |> Map.put("config", %{type: "test", test_string: "callback-fail"})
+
+      assert {:error, %Ecto.Changeset{}} = Mailings.create_sender(project.id, params)
+
+      assert [] == Mailings.get_project_senders(project.id)
+    end
   end
 
-  test "Delete senders" do
+  describe "Deleting senders" do
+    @tag :mailings
+    test "is idempotent" do
+      group = insert!(:group)
+      project = insert!(:project, group: group)
+      sender = insert!(:mailings_sender, project: project)
+
+      assert :ok == Mailings.delete_sender(sender.id)
+      assert :ok == Mailings.delete_sender(sender.id)
+    end
+
+    @tag :mailings
+    test "fails when there is a callback error" do
+      group = insert!(:group)
+      project = insert!(:project, group: group)
+
+      %{id: id} =
+        insert!(:mailings_sender,
+          project: project,
+          config: %{type: "test", test_string: "callback-fail"}
+        )
+
+      sender = Mailings.get_sender(id)
+
+      assert {:error, _} = Mailings.delete_sender(sender.id)
+      assert sender == Mailings.get_sender(sender.id)
+    end
   end
 
-  test "Update senders" do
+  @tag :mailings
+  describe "Update senders" do
+    @tag :mailings
+    test "through the API" do
+      group = insert!(:group)
+      project = insert!(:project, group: group)
+      sender = insert!(:mailings_sender, project: project)
+
+      config_params = %{"id" => sender.config.id, "type" => "test", "test_string" => "callback-success"}
+      params = %{"name" => "Updated name", "config" => config_params}
+      assert {:ok, %{name: "Updated name"}} = Mailings.update_sender(sender.id, params)
+    end
+
+    @tag :mailings
+    test "fails when there is a callback error" do
+      group = insert!(:group)
+      project = insert!(:project, group: group)
+      sender = insert!(:mailings_sender, project: project)
+
+      config_params = %{"id" => sender.config.id, "type" => "test", "test_string" => "callback-fail"}
+      params = %{"name" => "Updated name", "config" => config_params}
+      assert {:error, _changeset} = Mailings.update_sender(sender.id, params)
+    end
   end
 end
--- test/support/test_sender_adapter.ex
+++ test/support/test_sender_adapter.ex
@@ -5,11 +5,40 @@ defmodule Keila.TestSenderAdapter do
   def name, do: "test"
 
   @impl true
-  def schema_fields, do: []
+  def schema_fields, do: [test_string: :string]
 
   @impl true
-  def changeset(changeset, _params), do: changeset
+  def changeset(changeset, params) do
+    cast(changeset, params, [:test_string])
+  end
 
   @impl true
   def to_swoosh_config(_struct), do: []
+
+  @impl true
+  def after_create(sender) do
+    if sender.config.test_string == "callback-fail" do
+      {:error, "after_create callback failed"}
+    else
+      :ok
+    end
+  end
+
+  @impl true
+  def after_update(sender) do
+    if sender.config.test_string == "callback-fail" do
+      {:error, "after_update callback failed"}
+    else
+      :ok
+    end
+  end
+
+  @impl true
+  def before_delete(sender) do
+    if sender.config.test_string == "callback-fail" do
+      {:error, "before_delete callback failed"}
+    else
+      :ok
+    end
+  end
 end
