6a2918693417d7551efc5d4512e1bbe90e184395
Philipp <git@pentacent.com>
2021-06-11 10:29:47 +0200

    Refactor meta helper

--- lib/keila_web/helpers/meta/meta.ex
+++ lib/keila_web/helpers/meta/meta.ex
@@ -5,11 +5,16 @@ defmodule KeilaWeb.Meta do
 
   Must be used with `KeilaWeb.Meta.Plug`
 
-  Usage in controllers:
+  Usage in views:
+      def meta(assigns, "page.html", :title, _default) do
+        gettext("Title")
+      end
+
+  Usage in controllers (deprecated):
       put_meta(:title, gettext("Title"))
 
   Usage in templates:
-      get_meta(@conn, "Title", "Default")
+      get_meta(@conn, @view_module, @view_template, "Title", "Default")
   """
 
   @spec put_meta(Plug.Conn.t(), atom(), term()) :: Plug.Conn.t()
@@ -17,9 +22,13 @@ defmodule KeilaWeb.Meta do
     update_in(conn.private.keila_meta, &Map.put(&1, key, value))
   end
 
-  @spec get_meta(Plug.Conn.t(), atom(), term()) :: term()
-  def get_meta(conn, key, default \\ nil) do
-    # TODO Maybe rewrite as macro for better LiveView support?
-    Map.get(conn.private.keila_meta, key, default)
+  @spec get_meta(Plug.Conn.t(), atom(), atom(), atom(), term()) :: term()
+  # TODO Maybe rewrite as macro for better LiveView support?
+  def get_meta(conn, view_module, view_template, key, default \\ nil) do
+    if function_exported?(view_module, :meta, 3) do
+      view_module.meta(view_template, key, conn.assigns) || default
+    else
+      Map.get(conn.private.keila_meta, key, default)
+    end
   end
 end
