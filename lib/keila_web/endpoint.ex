9fff9a58469088d5e1c2857a348d49eae098a9d5
Philipp <git@pentacent.com>
2021-10-14 23:41:51 +0200

    Enable gzip

--- lib/keila_web/endpoint.ex
+++ lib/keila_web/endpoint.ex
@@ -23,7 +23,7 @@ defmodule KeilaWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :keila,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt downloads)
 
   # Code reloading can be explicitly enabled under the
