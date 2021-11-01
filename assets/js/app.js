01f105bb1b9e6e7eacc714e3c8c14a378ff37754
Philipp <git@pentacent.com>
2021-11-01 09:54:39 +0100

    Remove superfluous comment

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,3 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
 import 'alpinejs'
 
 // webpack automatically bundles all modules in your
