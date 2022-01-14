be1a5a4dc89e72b0096be4098b2e98452e9473fe
Philipp <git@pentacent.com>
2022-01-14 16:21:04 +0100

    Factor out JsonField into custom Ecto Type module

---
+++ lib/keila/repo/json_field.ex
@@ -0,0 +1,27 @@
+defmodule Keila.Repo.JsonField do
+  use Ecto.Type
+
+  def type, do: :map
+
+  def cast(string) when is_binary(string) do
+    case Jason.decode(string) do
+      {:ok, map} when is_map(map) ->
+        {:ok, map}
+
+      {:ok, _} ->
+        {:error, message: "must be a JSON object"}
+
+      {:error, error = %Jason.DecodeError{}} ->
+        message = Jason.DecodeError.message(error)
+        {:error, message: message}
+    end
+  end
+
+  def cast(map) when is_map(map), do: {:ok, map}
+
+  def dump(map) when is_map(map), do: {:ok, map}
+
+  def dump(_), do: :error
+
+  def load(map) when is_map(map), do: {:ok, map}
+end
