f6a4abb8ed4f95e823bfa4cd8091fb54a17b6551
Jan Petersen <11973633+JDPDO@users.noreply.github.com>
2022-04-15 09:26:39 +0000

    Deduplicate protocol implementations.

--- lib/keila/mailings/email/recipient.ex
+++ lib/keila/mailings/email/recipient.ex
@@ -1,33 +1,41 @@
-alias Keila.Contacts.Contact
 alias Swoosh.Email.Recipient
+
+alias Keila.Mailings.Campaign
+
+defimpl Recipient, for: Campaign do
+  @doc """
+  You can reply to campaigns. Format _reply to_-fields from campaigns.
+  """
+  def format(%Campaign{sender: value}), do: Recipient.format(value)
+end
+
+alias Keila.Contacts.Contact
 
 defimpl Recipient, for: Contact do
   def format(%Contact{email: address} = value) do
-    name = "#{value.first_name} #{value.last_name}"
-    {name, address}
+    Recipient.format({
+      "#{value.first_name} #{value.last_name}",
+      address
+    })
   end
+end
+
+alias Keila.Mailings.Recipient, as: KeilaRecipient
+
+defimpl Recipient, for: KeilaRecipient do
+  def format(%KeilaRecipient{contact: value}), do: Recipient.format(value)
 end
 
 alias Keila.Mailings.Sender
 
 defimpl Recipient, for: Sender do
-
   @doc """
   Format sender without _reply to_.
   """
   def format(%Sender{from_name: name, from_email: address}) do
-    {name, address}
-  end
-end
-
-alias Keila.Mailings.Campaign
-
-defimpl Recipient, for: Campaign do
-
-  @doc """
-  You can reply to campaigns. Format _reply to_-fields from campaigns.
-  """
-  def format(%Campaign{sender: %Sender{reply_to_name: name, reply_to_email: address}}) do
-    {name, address}
+    Recipient.format({
+      name,
+      address
+    })
   end
 end
