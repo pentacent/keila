e8355cbc2d9c5813c085396b99b5f6422acdf53f
Philipp <git@pentacent.com>
2022-09-14 21:54:54 +0200

    Add do-not-track option for campaigns

--- test/keila/mailings/mailings_builder_test.exs
+++ test/keila/mailings/mailings_builder_test.exs
@@ -64,6 +64,39 @@ defmodule Keila.Mailings.BuilderTest do
   end
 
   @tag :mailings_builder
+  test "Tracking can be enabled or disabled at the campaign-level" do
+    project = insert!(:project)
+    contact = insert!(:contact, project_id: project.id)
+    recipient = insert!(:mailings_recipient, contact: contact, project_id: project.id)
+    sender = build(:mailings_sender)
+
+    campaign =
+      insert!(:mailings_campaign,
+        project_id: project.id,
+        subject: "My Campaign",
+        sender: sender,
+        text_body: """
+        [Link](https://maybe-track.example.com)
+        """,
+        settings: %Mailings.Campaign.Settings{
+          type: :markdown
+        }
+      )
+
+    email = %Swoosh.Email{} = Mailings.Builder.build(campaign, recipient, %{})
+    {:ok, document} = Floki.parse_document(email.html_body)
+    # When tracking is enabled, the original link is not present
+    assert "https://maybe-track.example.com" not in Floki.attribute(document, "a", "href")
+
+    campaign =
+      Map.update!(campaign, :settings, fn settings -> %{settings | do_not_track: true} end)
+
+    email = %Swoosh.Email{} = Mailings.Builder.build(campaign, recipient, %{})
+    {:ok, document} = Floki.parse_document(email.html_body)
+    assert "https://maybe-track.example.com" in Floki.attribute(document, "a", "href")
+  end
+
+  @tag :mailings_builder
   test "Adds X-Keila-Invalid header when there is an error" do
     contact = build(:contact, id: Keila.Contacts.Contact.Id.cast(0) |> elem(1))
     sender = build(:mailings_sender)
