045dfbb555a9cd5fb9daeb270b3df150bca8f069
Philipp <git@pentacent.com>
2023-01-03 22:20:49 +0100

    Update Elixir to 1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Keila.MixProject do
     [
       app: :keila,
       version: "0.9.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
