5d5c0708e9bec5b2d7f8554e161f16cc2a2b42b9
Philipp <git@pentacent.com>
2023-09-24 15:17:48 +0200

    Add missing failed_at field to Recipients schema module

--- lib/keila/mailings/schemas/recipient.ex
+++ lib/keila/mailings/schemas/recipient.ex
@@ -11,6 +11,7 @@ defmodule Keila.Mailings.Recipient do
     field(:sent_at, :utc_datetime)
     field(:opened_at, :utc_datetime)
     field(:clicked_at, :utc_datetime)
+    field(:failed_at, :utc_datetime)
     field(:soft_bounce_received_at, :utc_datetime)
     field(:hard_bounce_received_at, :utc_datetime)
     field(:complaint_received_at, :utc_datetime)
