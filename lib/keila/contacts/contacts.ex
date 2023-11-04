726ce8205a2c363d486369de380759cb4db6750b
Philipp <git@pentacent.com>
2023-11-04 20:14:01 +0100

    Add FormAttrs alias

--- lib/keila/contacts/contacts.ex
+++ lib/keila/contacts/contacts.ex
@@ -6,7 +6,7 @@ defmodule Keila.Contacts do
   """
   use Keila.Repo
   alias Keila.Projects.Project
-  alias __MODULE__.{Contact, Import, Form, Segment}
+  alias __MODULE__.{Contact, Import, Form, FormAttrs, Segment}
   import KeilaWeb.Gettext
 
   @doc """
