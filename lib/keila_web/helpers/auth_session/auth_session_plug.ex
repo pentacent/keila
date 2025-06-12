752844c9a963a319ca398202dcf0298f9d70ed0f
Philipp <git@pentacent.com>
2025-06-12 12:10:47 +0200

    Add current_account assign to conn from AuthSession plug

--- lib/keila_web/helpers/auth_session/auth_session_plug.ex
+++ lib/keila_web/helpers/auth_session/auth_session_plug.ex
@@ -1,4 +1,5 @@
 defmodule KeilaWeb.AuthSession.Plug do
+  alias Keila.Accounts
   alias Keila.Auth
   import Plug.Conn
 
@@ -9,14 +10,20 @@ defmodule KeilaWeb.AuthSession.Plug do
   def call(conn, _) do
     with session_token when is_binary(session_token) <- get_session(conn, :token),
          token = %Auth.Token{} <- Auth.find_token(session_token, "web.session"),
-         user = %Auth.User{} <- Keila.Repo.get(Auth.User, token.user_id) do
+         user = %Auth.User{} <- Auth.get_user(token.user_id),
+         account <- Accounts.get_user_account(user.id) do
       is_admin? = Auth.has_permission?(user.id, Auth.root_group().id, "administer_keila")
 
       conn
       |> assign(:current_user, user)
+      |> assign(:current_account, account)
       |> assign(:is_admin?, is_admin?)
     else
-      _ -> assign(conn, :current_user, nil)
+      _ ->
+        conn
+        |> assign(:current_user, nil)
+        |> assign(:current_account, nil)
+        |> assign(:is_admin?, false)
     end
   end
 end
