58018b936c67149ccae654625029c4577eb58b07
Philipp <git@pentacent.com>
2026-04-01 21:30:43 +0200

    Add target=_blank to UI Markdown links when they point to external sites

--- lib/keila_web/gettext/markdown_helpers.ex
+++ lib/keila_web/gettext/markdown_helpers.ex
@@ -22,8 +22,18 @@ defmodule KeilaWeb.Gettext.MarkdownHelpers do
   defmacro dgettext_md(domain, msgid, bindings \\ Macro.escape(%{})) do
     quote do
       dgettext(unquote(domain), unquote(msgid), unquote(bindings))
-      |> Earmark.as_html!()
+      |> Earmark.as_html!(
+        registered_processors: {"a", &KeilaWeb.Gettext.MarkdownHelpers.add_target/1}
+      )
       |> then(fn html -> {:safe, html} end)
+    end
+  end
+
+  def add_target(node) do
+    if Earmark.AstTools.find_att_in_node(node, "href", "") =~ ~r/^https?:\/\// do
+      Earmark.AstTools.merge_atts_in_node(node, target: "_blank")
+    else
+      node
     end
   end
 end
